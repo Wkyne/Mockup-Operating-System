17330e34857aa72168d7b056e89718699558dc67
Wkyne <djbgranada2017@gmail.com>
2025-10-19 12:58:43 +0800

    initial branch

---
+++ src/main/java/com/gummybear/filemanagement/FileManager.java
@@ -0,0 +1,15 @@
+package com.gummybear.filemanagement;
+
+public class FileManager {
+    
+
+
+
+    void uploadFolder(String folderPath) {
+
+    }
+
+    void deleteFolder(String folderPath) {
+
+    }
+}
